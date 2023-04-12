76153e9f643df8871e3d96bfd8986e7e00393ebf
Eise <eise.booij@gmail.com>
2023-04-12 17:13:54 +0200

    Added Prompt UI

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+    void Start()
+    {
+        cameraToLookAt= Camera.main;
+        //transform.Rotate( 180,0,0 );
+    }
+
+    void Update()
+    {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+
+        v.x = v.z = 0.0f;
+
+        transform.LookAt(cameraToLookAt.transform.position - v);
+
+        transform.rotation = (cameraToLookAt.transform.rotation);
+    }
+}
